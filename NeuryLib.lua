4096928328d9bd848e399401c90f1c59ae2c3b6a
iXayemiz <guest45542008@gmail.com>
2026-09-05 12:50:57 -0400

    Adjust Sidebar size and position in NeuryLib

--- NeuryLib.lua
+++ NeuryLib.lua
@@ -135,7 +135,7 @@ function Library.new(hubName, accentColor)
 
 	local Sidebar = Instance.new("Frame")
 	Sidebar.Name = "Sidebar"
-	Sidebar.Size = UDim2.new(0, 195, 1, -55)
+	Sidebar.Size = UDim2.new(0, 200, 1, -55)
 	Sidebar.Position = UDim2.new(0, 0, 0, 55)
 	Sidebar.BackgroundTransparency = 1
 	Sidebar.Parent = MainFrame
@@ -143,7 +143,7 @@ function Library.new(hubName, accentColor)
 
 	local SidebarDivider = Instance.new("Frame")
 	SidebarDivider.Size = UDim2.new(0, 1, 1, -55)
-	SidebarDivider.Position = UDim2.new(0, 205, 0, 55)
+	SidebarDivider.Position = UDim2.new(0, 200, 0, 55)
 	SidebarDivider.BackgroundColor3 = Color3.fromRGB(24, 24, 30)
 	SidebarDivider.BorderSizePixel = 0
 	SidebarDivider.Parent = MainFrame
